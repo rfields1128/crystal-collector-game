4cf3f79c9c35b8f3f493755b3ac446958023d2ea
Randall <rfields1128@yahoo.com>
2019-09-11 19:00:15 -0500

    entered in psuedocode and variables for javascript

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,26 @@
+// generate a random number and asign it to a variable
+var targetNumber
+var totalScore
+// create variables to hold wins, loses
+var winsTotal
+var lossTotal
+// display each crystal
+var redCrystal
+var blueCrystal
+var yellowCrystal
+var greenCrystal
+
+// add a onclick event listener to each crystal
+
+// each  crystal in itself has a random number 
+
+
+// when ever the  user clicks a crystal, the total score is updated
+
+
+// check to see if total score is greater than random number,
+//if it is then updated losses
+
+
+// check to see if  total score is eqal to random number, 
+//if it is then update wins. 
